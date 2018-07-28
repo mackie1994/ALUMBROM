3999cf60a22c007f86ea399b9492523f5a7f196d
mackie1994 <mcstephenalumbro@gmail.com>
2018-07-28 12:37:24 +0800

    Create ALUMBROM.php

---
+++ ALUMBROM.php
@@ -0,0 +1,104 @@
+<html>
+
+
+
+<head>
+
+<title>Mc Stephen M. Alumbro</title>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<style type="text/css" media="screen">
+
+	@import url(http://alexking.org/site/projects/html-resume-template/demo/resume.php?output=css_screen);
+
+</style>
+
+<link rel="stylesheet" type="text/css" media="print" href="http://alexking.org/site/projects/html-resume-template/demo/resume.php?output=css_print" />
+
+<script type="text/javascript" src="http://alexking.org/site/projects/html-resume-template/demo/resume.php?output=js"></script>
+
+</head>
+
+
+
+
+
+<body>
+
+
+
+
+
+<h1>Profile</h1>
+
+
+
+<p id="bio_left">Name: Mc Stephen M. Alumbro<br /> Adress: Dapitan City	<br /> Gender: Male <br /> Age: 24 
+
+</p>
+
+
+
+<p id="bio_right">
+
+<img src="http://localhost/img/dummy-profile.png" /></p>
+
+
+
+
+
+<h2>Goal</h2>
+
+
+
+<ul>
+
+<li>As a senior contributor, you are not only an SME, but a mentor to junior team members. You also lead/develop internal documentation for processes and procedures. Create check lists so no stone goes unturned during critical analysis and processes (like major production deployments.)</li>
+
+
+
+
+
+
+
+
+
+</ul>
+
+<h2>Objective</h2>
+
+<ul>
+
+<li>As mentioned already, most Software Developers are highly motivated to deliver features, products, and business value. The answer to your question depends on what division you’re a part of: Feature, Enterprise, Core, or Back Office.</li>
+
+
+
+
+
+
+
+</ul>
+
+<h2>About Me</h2>
+
+
+
+<ol>
+
+<li>Im a programmer from Dapitan City</li>
+
+
+
+
+
+
+
+</ol>
+
+</body>
+
+
+
+</html>
+
